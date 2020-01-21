1134964ca229a89c44765e0a0c779badc3b11969
clamyang <ybq2888@163.com>
2020-01-21 01:38:23 -0800

    delete unused dir

--- fluent_python/1_Python数据模型/namedtuple.py
+++
@@ -1,6 +0,0 @@
-from collections import namedtuple
-# 创建一个具名元组需要两个参数
-# 一个是类名，另一个是类的各个字段的名字。
-City = namedtuple('City', ['name', 'country', 'population', 'coordinates'])
-tokyo = City('Tokyo', 'Jp', 36.933, (35.689722, 139.691667))
-print(tokyo)
